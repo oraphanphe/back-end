d3efdb285105e0e8c9212531ed8e55b48e31d9eb
oraphanphe <89006939+oraphanphe@users.noreply.github.com>
2021-09-14 23:14:55 +0700

    Delete wsTaxSpring directory
    delete test

--- wsTaxSpring/src/main/java/ws/personnel/tax/repository/TaxCatalogRepository.java
+++
@@ -1,8 +0,0 @@
-package ws.personnel.tax.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ws.personnel.tax.entities.TaxCatalog;
-@Repository
-public interface TaxCatalogRepository extends JpaRepository<TaxCatalog, String>{
-
-}
